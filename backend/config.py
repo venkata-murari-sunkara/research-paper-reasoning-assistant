b3830dc9fc1d470b77c7fb1bc0c4423738bf3930
venkata-murari-sunkara <venkatamurari32@gmail.com>
2026-06-14 19:12:12 +0530

    Update paths for deployment

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,4 @@
 import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,6 +8,9 @@ API_KEY = os.getenv("OPENAI_API_KEY")
 embed_model = "BAAI/bge-small-en-v1.5"
 llm_model = "gpt-4o-mini"
 
-data_dir = "./research-data"
-chroma_db_dir = "./chroma_db"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+data_dir = os.path.join(BASE_DIR, "research-data")
+chroma_db_dir = os.path.join(BASE_DIR, "chroma_db")
+
 collection_name = "research_papers"
