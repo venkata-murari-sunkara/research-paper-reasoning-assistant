3b1485c007c75f77f202e42d14838830b2d249f0
venkata-murari-sunkara <venkatamurari32@gmail.com>
2026-06-14 18:55:18 +0530

    Changes made in app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import requests
+import os
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
 
 st.set_page_config(
     page_title="Research Paper Reasoning Assistant",
